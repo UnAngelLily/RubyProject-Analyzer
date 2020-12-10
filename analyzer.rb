5d4175da06303870c82f2f401a43e27927452ce6
UnAngelLily <unangellily@gmail.com>
2020-12-10 09:19:49 -0700

    finished named analyzer

--- analyzer.rb
+++ analyzer.rb
@@ -2,3 +2,8 @@ puts "What is your first name?"
 first_name = gets.chomp
 puts "What is your last name?"
 last_name = gets.chomp
+full_name = "#{first_name} #{last_name}"
+letter_count = first_name.length + last_name.length
+puts "Your full name is #{full_name}\n
+There are " + "#{letter_count}" + " in your full name. \n
+Your full name backwards is #{full_name.reverse} "
